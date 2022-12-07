bd9dd7c2877304b5a4f921a2a34d0ba212900c38
yixellet <yixwellet@gmail.com>
2022-12-07 21:28:13 +0400

    t

--- src/features/NewCpForm/NewCpForm.jsx
+++ src/features/NewCpForm/NewCpForm.jsx
@@ -7,6 +7,19 @@ function NewCpForm() {
   return (
     <div className={styles.newcp_form_wrapper}>
       <h1 className={styles.newcp_header}>Новый контрагент</h1>
+      <div className={styles.type_radio_wrapper}>
+        <div className={styles.type_radio_with_label}>
+          <input className={styles.radio} type="radio" id="entity" name="type" value="entity" checked />
+          <label for="entity">Юридическое лицо</label>
+        </div>
+        <div className={styles.type_radio_with_label}>
+          <input className={styles.radio} type="radio" id="person" name="type" value="person" />
+          <label for="person">Физическое лицо</label>
+        </div>
+      </div>
+      <form className={styles.newcp_form}>
+
+      </form>
     </div>
   )
 };
